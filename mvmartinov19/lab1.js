ad5f83857220428261c55cd8bbbe1f517bc92ba4
Anton <mvmartinov19@codingburgas.bg>
2021-03-22 09:59:00 +0200

    adding

--- mvmartinov19/lab1.js
+++ mvmartinov19/lab1.js
@@ -1,11 +1,11 @@
 function calc(a, b) {
-    return a * b;
-  }
-  
-  function foo() {
-  
-  }
-  
-  console.log(calc(10,5));
-  console.log(calc(10));
-  console.log(foo());
+  return a * b;
+}
+
+function foo() {
+
+}
+
+console.log(calc(10,5));
+console.log(calc(10));
+console.log(foo());
